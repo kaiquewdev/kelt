9298c354c757d88f555cbd62599caf3166bbc316
Kaique da Silva <kaique.developer@gmail.com>
2012-09-19 23:33:37 +0000

    test passing

--- kelt/tests/module.test.js
+++ kelt/tests/module.test.js
@@ -20,11 +20,19 @@ suite('Kelt', function () {
 
     suite('Command action', function () {
         test('undefined action', function () {
-            expect( kelt.action('test') ).to.be.equal(undefined);    
+            expect( kelt.command.action('test') ).to.be.equal(undefined);    
         });
         
         test('set action', function () {
-            
+            expect( kelt.command.action('test', function () { return 'this is a test!' }) ).to.be.true;    
+        });
+
+        test('get and exec action', function () {
+            expect( kelt.command.action('test').exec() ).to.be.equal('this is a test!');    
+        });
+
+        test('false action', function () {
+            expect( kelt.command.action() ).to.be.false;    
         });
     });
 });
