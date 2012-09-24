fd11150b1b3069e5df9142a04a87801f7824eb20
Kaique da Silva <kaique.developer@gmail.com>
2012-09-24 04:31:10 +0000

    cli working for basix

--- kelt/cli.js
+++ kelt/cli.js
@@ -8,20 +8,88 @@ kelt.command.prefix('--');
 
 // Help command
 kelt.command.action('help', function () {
-    console.log('Kelt cli version 0.0.1');    
+    console.log('Kelt cli version 0.0.1 \n');
+
+    vodevil.intersect( kelt.parameters.list({ prefix: true }), function ( content, id ) {
+        var parameters = kelt.parameters.list({ prefix: false });
+        var help = kelt.parameter.help( parameters[ id ] );
+
+        console.log( ' %s - %s \n', content, help );
+    });
 });
 
-vodevil.intersect( kelt.parameters.list({ prefix: true }), function ( content, id ) {
-    var parameters = kelt.parameters.list({ prefix: false });
+kelt.parameter.help('help', 'helt to use this cli.');
 
-    if ( content === args[0] ) {
-        var command = kelt.command.action( parameters[ id ] );
+// Clone action
+kelt.command.action('clone', function ( source, destiny ) {
+    kelt.template.clone( source, destiny );
+});
 
-        if ( 'undefined' !== command ) {
-            command.exec();    
-        } 
-    } else {
-        console.log('Kelt version 0.0.1 \n');
-        console.log('type --help, for more information.');
+kelt.parameter.help('clone', 'clone a template, from the source to your destiny.');
+
+// Start action
+kelt.command.action('start', function () {
+    var fs = require('fs'),
+        source = './keltc.json',
+        options = {
+            main: ''
+        };
+
+    if ( source ) {
+        fs.writeFileSync(
+            source,
+            JSON.stringify( options )
+        );
+
+        if ( fs.existsSync( source ) ) {
+            console.log(
+                'Started, check the file: %s, and insert' +
+                'the path of template directory!', 
+                source
+            );
+        }
     }
 });
+
+kelt.parameter.help('start', 'Create the config file.');
+
+// Unrecognized action
+var unrecognize = function () {
+    console.log('Kelt version 0.0.1 \n');
+    console.log('use --help, to have more information, about this cli.');
+};
+
+// prefixed parameters
+var prefixParameters = kelt.parameters.list({
+    prefix: true    
+});
+
+// Main slice if cli
+vodevil.intersect( prefixParameters, function ( content, id ) {
+    var fs = require('fs'),
+        parameters = kelt.parameters.list({ prefix: false }),
+        currentParameter = parameters[ id ],
+        command = kelt.command.action( currentParameter );
+
+    if ( undefined !== command && content === args[0] ) {
+        if ( 'clone' === currentParameter ) {
+            if ( fs.existsSync( './keltc.json' ) ) {
+                var config = JSON.parse( fs.readFileSync( './keltc.json' ) );
+                
+                if ( fs.existsSync( config.main ) ) {
+                    config['main'] = ( config.main.search('\/$') > -1 && config.main ) || config.main + '/';
+                    command.exec( config.main + args[1], args[2] );
+                }
+            } else {
+                command.exec( args[1], args[2] ); 
+            }
+        } else {
+            command.exec();    
+        }
+    } 
+});
+
+// Warning message if not have a typed parameter in the parameters list
+if ( !vodevil.in( prefixParameters, args[0] ) ) {
+    unrecognize();
+}
